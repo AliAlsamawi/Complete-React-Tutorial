f853ae371164b3686a85681ae4f88e8322c535ee
Ali A <alsamawiali4@gmail.com>
2021-12-05 18:56:10 -0500

    props

--- ali-blog/src/Home.js
+++ ali-blog/src/Home.js
@@ -1,24 +1,20 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 const Home = () => {
-  // let name = "Ali"
-  const[name, setName]= useState('mario');
-  const[age,setAge]= useState(25);
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-
-  const handleclick = ()=>{
-    setName('Love');
-    setAge('30')
-    
-
-  }
-
-  return ( 
+  return (
     <div className="home">
-      <h2>Home Page</h2>
-      <p> {name} is {age} year old </p>
-      <button onClick={handleclick}>Click me</button>
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
